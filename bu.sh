116656d3348ca0853e615c825410a48374158710
Andrew Black <andrew@black1.org.uk>
2019-09-12 15:44:48 +0100

    add to backup

--- bu.sh
+++ bu.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 
 
-alias cx='cp -v -t ~/backups' 
+function cx {
+  FILE=$1 
+  echo ---------- $FILE
+  cp -v -t ~/backups $FILE
+  pushd ~/backups
+  git commit $FILE -m"update $FILE"
+  popd 
+}
 
-cx ~/add_alias
-cx ~/.bash_aliases
+cd ~
+cx add_alias
+pwd
+cx .bash_aliases
+cx .bookmarks.data
 
-cd ~/backups
-git commit .b* -m'update'
-git commit * -m'update'
+
 
