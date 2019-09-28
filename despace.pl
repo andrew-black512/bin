c7c770e28067d5861ea85d29ebb780b03889773f
Andrew Black <andrew@black1.org.uk>
2019-09-28 12:04:06 +0100

    concert multiple puncs to one space

--- despace.pl
+++ despace.pl
@@ -3,6 +3,10 @@
 
 foreach my $filename (@ARGV) {
   my $newfilename = $filename ;
+  
+  # multiple punctuation to one
+    $newfilename =~ s/ \s+ ([-]) \s+/$1/gx ;
+  # Multiple spaces 
   $newfilename =~ s/ \s+ /_/gx ;
   print "Rename $filename as  $newfilename\n" ;
   rename $filename , $newfilename or die "Cannot rename file: $!";
