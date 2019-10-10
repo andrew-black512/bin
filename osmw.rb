4256bf0c2147032624ebc3e8063503381d361318
Andrew Black <andrew@black1.org.uk>
2019-10-11 00:49:30 +0100

    new file:   osmw.rb

---
+++ osmw.rb
@@ -0,0 +1,7 @@
+# https://wiki.openstreetmap.org/wiki/Key:amenity
+# https://wiki.openstreetmap.org/wiki/Tag:amenity%3Dpub
+
+(key,value)=ARGV
+puts key 
+system ( "gnome-open https://wiki.openstreetmap.org/wiki/Tag:amenity%3Dpub " ) 
+
