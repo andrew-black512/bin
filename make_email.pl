da249530f21f76e52faf52bd5f415b54a746f80a
Andrew Black <andrew@black1.org.uk>
2020-02-24 15:29:22 +0000

    Add http//

--- make_email.pl
+++ make_email.pl
@@ -1,12 +1,19 @@
 my $domain ;
 while (<>) {
-   if ( /@(\S*)/ )  {
+   if ( /@(\S*)/x )  {
       $domain = $1 ;
-      print "domain = $domain \n" ; 
+      print "\n" ;
+      print "domain = $domain \n" ;
+   } elsif ( m| // ( [^/]* ) |x )  {
+      $domain = $1 ;
+       $domain =~ s/^www\.//g ;
+      print "\n" ;
+      print "domain = $domain \n" ;
    } else {
        chomp ;
        my $name = $_ ;
        $name =~ s/\s*$//g ;
+       $name =~ s/^\s*//g ;
        $name =~ s/ /./g ;
        print "$name\@$domain\n" ;
 
