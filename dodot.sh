eaba2c51b8447c445f0a5ff936f93ca32fce0f48
Andrew Black <andrew@black1.org.uk>
2020-09-02 14:08:10 +0100

    select basename

--- dodot.sh
+++ dodot.sh
@@ -1,7 +1,7 @@
 #TODO - only use out dir if it exists
 ##   perl station2dot.pl LBG_PUR > out/L.do
 
-FILEBASE=$1
+FILEBASE=`basename $1 .dot`
 PDF=out/${FILEBASE}.pdf
 
    dot ${FILEBASE}.dot -Tpdf > ${PDF}
