14b13e573f330400dbf60a1643bad74af282d2fb
Andrew Black <andrew@black1.org.uk>
2020-09-14 16:33:56 +0100

    Add TODOs

--- dodot.sh
+++ dodot.sh
@@ -1,8 +1,12 @@
 #TODO - only use out dir if it exists
+#TODO - should it use out in directory pf dot file or current dir?
 ##   perl station2dot.pl LBG_PUR > out/L.do
 
+#TODO handle other directoires. Either use regexp to removed or compose from dirname.
 FILEBASE=`basename $1 .dot`
 PDF=out/${FILEBASE}.pdf
 
    dot ${FILEBASE}.dot -Tpdf > ${PDF}
+   #TODO handle error - dont open.
+
    gnome-open ${PDF}
