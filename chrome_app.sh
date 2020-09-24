9b6c8a1dcdb7126d4e7d3eaac25ea249869ad6f6
Andrew Black <andrew@black1.org.uk>
2020-09-24 09:44:05 +0100

    add stuff

--- chrome_app.sh
+++ chrome_app.sh
@@ -1,1 +1,15 @@
-chromium-browser --app=https://messages.google.com/web/
+URL=$1
+OPT=$2
+ALIAS=$3
+
+echo "$ALIAS='chromium-browser --app=$URL $OPT &'"
+#chromium-browser --app=$URL $OPT &
+
+
+exit 
+dont get here
+
+# notes
+andrew@Tallis:~/bin$ chrome_app.sh http://192.168.1.1 --user_data_dir=~/scratch/chromium_user
+
+
