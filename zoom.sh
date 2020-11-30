4c7f186db5d43af6b25eb6fc52a68b15c3b2fe1b
Andrew Black <andrew@black1.org.uk>
2020-11-30 19:47:52 +0000

    new file:   zoom.sh

---
+++ zoom.sh
@@ -0,0 +1,12 @@
+cat <<-ENDOFMESSAGE
+	Before starting zoom
+	 - get monitor config right
+	 - decide on which headphones and plug them in
+
+ENDOFMESSAGE
+gnome-control-center sound &
+gnome-open /home/andrew/Downloads/backdrops/sheep_sd.jpg &
+zoom &
+
+
+
