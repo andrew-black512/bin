14c271c12764eef708b16c3ee018836b7144bab3
Andrew Black <andrew@black1.org.uk>
2021-05-31 22:07:16 +0100

    ass clipl

--- aliase_clip.sh
+++ aliase_clip.sh
@@ -2,3 +2,4 @@ echo bash alias with cdd + extras + up + mvs xclip mustools
 #  pipe | clip or clip >file>
     alias clip='xclip -i -selection clipboard'
     alias clips='xclip -o -selection clipboard'
+    alias clipl='xclip -o -selection clipboard | less'
