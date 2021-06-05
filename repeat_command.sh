fa7d1f5c9f5c7e0e49425dc0da255531f83450f6
Andrew Black <andrew@black1.org.uk>
2021-06-05 16:58:04 +0100

    add ping

--- repeat_command.sh
+++ repeat_command.sh
@@ -1,4 +1,10 @@
 #repeat
-NODE=127.0.0.1
+while :; do
+
+NODE=ec2-3-235-83-1.compute-1.amazonaws.com:h
+NODE=j.aimless.thn.aa.net.uk
     echo ==== $(date)
     traceroute  $NODE
+    sleep 5
+
+done
