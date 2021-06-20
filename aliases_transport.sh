36b46a5838bbec9d3ec056c2c39f2aac0c8ec038
Andrew Black <andrew@black1.org.uk>
2021-06-20 23:15:21 +0100

    flesh out busrep in   aliases_transport.sh

---
+++ aliases_transport.sh
@@ -0,0 +1,12 @@
+TRANPORT=/home/andrew/dev/trainsstable
+
+function busrep {
+STOP=$1
+  echo $STOP
+  for I in {1..20} ;do
+    $TRANPORT/busexp.rb $STOP
+    sleep 30
+  done
+}
+alias stop_g='busrep 490007299G'
+alias stop_h='busrep 490007299H'
