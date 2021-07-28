a0c60c72700fd4cbd383e4bfbf0f6fe96a59bdb9
Andrew Black <andrew@black1.org.uk>
2021-07-28 17:38:47 +0100

    vaious git.xx

--- aliases_git.sh
+++ aliases_git.sh
@@ -1,15 +1,24 @@
+#Command that I have trouble
+#  remembering. or
+#  typing (too many --)
+#
+# Moving toward
+#  Common ones - no punct
+#  others have a . (to avoid confusing with  eg git-receive-pack)
 
 alias gits='git  status '
-alias gitl='git log --oneline --decorate'
+alias gitl='echo ;git log --oneline --decorate --graph'
 
-alias gitcl='git clean -n'
+alias git.cl='git clean -n'
 
 # list ignored files. Otere ways of doing this
-alias gitig='git status --short --ignored'
+alias git.ig='git status --short --ignored'
 
 # commit (-p selects huns, -v puts diff into editor, -u no : dont display untracked)
    alias gitcp='git commit -p -v -uno'
 
+# This is a dangerous command to have in command histroy.
+# TODO - put a check in first.
 function gitchk {
 FILE=$1
   #TODO Check for file
