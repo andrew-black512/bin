38d888ffae250d4254ca40a149415ce042a8e6ca
Andrew Black <andrew@black1.org.uk>
2021-08-01 17:51:59 +0100

    prompt basically works

--- prompt.rb
+++ prompt.rb
@@ -1,1 +1,12 @@
-git symbolic-ref --short HEAD echo -
+#!/usr/bin/ruby
+
+out = []
+branch =`git symbolic-ref --short HEAD`
+branch.chomp!
+branch = '-' if branch =~ /fatal:/
+out.push  branch
+# todo - change to ~ and truncate...
+out.push Dir.pwd
+out.push '$ '  # $ reminds me of DCL
+
+print out.join ' '
