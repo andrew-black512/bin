fed218f1219e8c019b746c956ea7d9b61c2ce746
Andrew Black <andrew@black1.org.uk>
2021-08-01 18:16:45 +0100

    color prompt

--- prompt.rb
+++ prompt.rb
@@ -1,14 +1,20 @@
 #!/usr/bin/ruby
+require 'colorize'
 #TODO - try $?.success?
 #  https://stackoverflow.com/questions/18728069/ruby-system-command-check-exit-code
+
+# semi config
+branch_special = { 'master' => 1, 'develop'=>1}
+# Main
 
 out = []
 branch =`git symbolic-ref --short HEAD`
 branch.chomp!
 branch = '-' if branch =~ /fatal:/
+branch = branch.red if branch_special[branch]
 out.push  branch
 # todo - change to ~ and truncate...
-out.push Dir.pwd
+out.push Dir.pwd.light_green
 out.push '$ '  # $ reminds me of DCL
 
 print out.join ' '
