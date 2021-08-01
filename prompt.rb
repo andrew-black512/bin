d424af7b1c0c1023919edcd8f40237f431f5765e
Andrew Black <andrew@black1.org.uk>
2021-08-01 20:48:55 +0100

    test for success

--- prompt.rb
+++ prompt.rb
@@ -8,13 +8,18 @@ branch_special = { 'master' => 1, 'develop'=>1}
 # Main
 
 out = []
-branch =`git symbolic-ref --short HEAD`
-branch.chomp!
-branch = '-' if branch =~ /fatal:/
-branch = branch.red if branch_special[branch]
+branch =`git branch`
+if $?.success?
+  branch.sub! '* ',''
+  branch.chomp!
+  branch = branch.upcase if branch_special[branch]
+else
+  branch = 'no git'
+end
+
 out.push  branch
 # todo - change to ~ and truncate...
-out.push Dir.pwd.light_green
+out.push Dir.pwd  #.light_green
 out.push '$ '  # $ reminds me of DCL
 
 print out.join ' '
