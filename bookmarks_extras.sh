432d585e67305d2dfd4570c26e77332a57ca0019
Andrew Black <andrew@black1.org.uk>
2021-08-02 11:12:10 +0100

    find bookmarks to current dir (if any)

--- bookmarks_extras.sh
+++ bookmarks_extras.sh
@@ -1,3 +1,9 @@
 alias br='bookmarks -r bash'
 alias bh='bookmarks |less'
 alias pd='pushd'
+
+# looks for bookmarks for current directory (or subdirectory)
+# TODO make function and add param
+alias b1f='grep $(readlink -f ..) ~/.bookmarks.data'
+alias bf2='grep $(readlink -f ../..) ~/.bookmarks.data'
+alias bf='grep $(readlink -f .) ~/.bookmarks.data'
