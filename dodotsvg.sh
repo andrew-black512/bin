92523bf67caa69cb08362f065b34e152ac868817
Andrew Black <andrew@black1.org.uk>
2021-08-03 18:10:52 +0100

    deleted:    dodotsvg.sh

--- dodotsvg.sh
+++
@@ -1,8 +0,0 @@
-#TODO - only use out dir if it exists
-##   perl station2dot.pl LBG_PUR > out/L.do
-
-FILEBASE=`basename $1 .dot`
-PDF=out/${FILEBASE}.pdf
-
-   dot ${FILEBASE}.dot -Tpdf > ${PDF}
-   gnome-open ${PDF}
