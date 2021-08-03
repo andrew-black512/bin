9105c5faba751e69ea0fb5b1dc04ce954859c490
Andrew Black <andrew@black1.org.uk>
2021-08-03 17:42:51 +0100

    modified:   aud_test.sh

--- aud_test.sh
+++ aud_test.sh
@@ -1,3 +1,8 @@
+ANO=~/bin/aud_test.wav
 FILE=/tmp/soxrecording.wav
+
+play $ANO 
+# TODO make sensible name
+echo Recording 
  sox -b 32 -e unsigned-integer -r 96k -c 2 -d --clobber --buffer $((96000*2*10)) $FILE trim 0 3
 play $FILE
