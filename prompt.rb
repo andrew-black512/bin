82b1a2a20ec16dbb5b5e9590d59ae489c5450d87
Andrew Black <andrew@black1.org.uk>
2021-08-03 14:54:27 +0100

    return to color

--- prompt.rb
+++ prompt.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/ruby
-require 'colorize' # causes problems with prompt...
+require '/home/andrew/exp/ruby/color.rb' # causes problems with prompt...
+
+def prompt_wd
+  # Print last two directories pwd
+  dirs = Dir.pwd.split '/'
+  return  dirs[-2] +'/' + dirs[-1]
+
+
+end
 
 # semi config
 branch_special = { 'master' => 1, 'develop'=>1}
@@ -10,14 +18,16 @@ branch =`git symbolic-ref --short HEAD 2>/dev/null`
 if $?.success?
   branch.sub! '* ',''
   branch.chomp!
-  branch = branch.upcase if branch_special[branch]
+  #branch = branch.upcase if branch_special[branch]
+  branch = branch.colorize(:red) if branch_special[branch]
+
 else
-  branch = 'no-git'
+  branch = 'no-git'.colorize(:gray)
 end
 
 out.push  branch
 # todo - change to ~ and truncate...
-out.push Dir.pwd  #.light_green
+out.push prompt_wd.colorize(:green)
 out.push '$ '  # $ reminds me of DCL
 
 print out.join ' '
