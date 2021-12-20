6351bf7e3a715d7faf94e257a583122ed316d2c3
Andrew Black <andrew@black1.org.uk>
2021-12-20 12:06:01 +0000

    add tl

--- aliases_pro_tem.sh
+++ aliases_pro_tem.sh
@@ -3,3 +3,8 @@
 echo TODO move phdir eg tools
 alias phdir='perl /home/andrew/wd/perllibold/phdir.pl'
 
+alias timer='ruby /home/andrew/exp/ruby/lib/windowcontrol.rb'
+
+
+alias tl=' dist -dt thought_log '
+alias tlh='echo F: fear P: perfect'
