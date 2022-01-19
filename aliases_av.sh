5fc701a9440f8b34244fc7b50d009498d7cef81f
Andrew Black <andrew@black1.org.uk>
2022-01-19 19:16:30 +0000

    add firefox and chrome.  Make meet a fumction to allow &

--- aliases_av.sh
+++ aliases_av.sh
@@ -1,6 +1,18 @@
 # Meet
   export MEET=/home/andrew/work/meet
-  alias meet='meet.pl'
-
+  function meet {
+    meet.pl "$*" &
+  }
+#browsers
+ function cf  {
+   cd /home/andrew/dev/tools/
+   ack =\>  firefox.pl chrome.pl
+ }
+ function ch {
+   ( chrome.pl "$*" || cf )  &
+ }
+ function ff {
+   firefox.pl "$*" &
+ }
 # network
   alias pg="ping     www.google.com -c4"
