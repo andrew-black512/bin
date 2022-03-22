8a80cc4ad365f8228e1ba2eaeb701f12ab27d615
Andrew Black <andrew@black1.org.uk>
2022-03-22 11:32:20 +0000

    new file:   aliases_chat.sh

---
+++ aliases_chat.sh
@@ -0,0 +1,4 @@
+/home/andrew/.bash_aliases
+62:    export CALM=~/work/calm
+116:alias calm='perl  /home/andrew/wd/perllib/calm.pl'
+117:alias calmu='perl -ibak/*.bak /home/andrew/wd/perllib/calm.pl'
