7c4e050d57d9126e10e7f15c836a988587965fcb
Andrew Black <andrew@black1.org.uk>
2022-03-26 11:45:27 +0000

    correct bf1

--- bookmarks_extras.sh
+++ bookmarks_extras.sh
@@ -4,9 +4,17 @@ THISFILE=$(readlink -f ~/bin/bookmarks_extras.sh)
 alias br='bookmarks -r bash'
 alias bh="(bookmarks;cat $THISFILE) |less"
 alias pd='pushd'
+alias po='popd'
+function sw {    # swap.
+  pushd
+}
+function dv {
+    echo first two swap with pushd pd or sw
+    dirs -v
+}
 
 # looks for bookmarks for current directory (or subdirectory)
 # TODO make function and add param
-alias b1f='grep $(readlink -f ..) ~/.bookmarks.data'
+alias bf1='grep $(readlink -f ..) ~/.bookmarks.data'
 alias bf2='grep $(readlink -f ../..) ~/.bookmarks.data'
 alias bf='grep $(readlink -f .) ~/.bookmarks.data'
