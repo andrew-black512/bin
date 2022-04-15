4db462c069dce3236edbcba882035795288fd623
Andrew Black <andrew@black1.org.uk>
2022-04-15 22:24:47 +0100

    add clipd (date)

--- aliases_clip.sh
+++ aliases_clip.sh
@@ -1,4 +1,9 @@
 echo bash alias with cdd + extras + up + mvs xclip mustools
+function clipd() {
+OFFSET=$1
+echo -n $(date.py $OFFSET) | xclip -i -selection clipboard
+}
+
 #  pipe | clip or clip >file>
     alias clip='xclip -i -selection clipboard'
     alias clips='xclip -o -selection clipboard'
