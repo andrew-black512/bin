eb7121b76238cfc0dfc37097e725b5ee08795dd3
Andrew Black <andrew@black1.org.uk>
2022-04-19 18:05:03 +0100

    add comments

--- gitdl.sh
+++ gitdl.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Does DIFF and Brief LOG.
+# Log is to cover 2 cases
+#   - file not in git
+#   - file recently changed and I have forgotten that I have commited.
 FILE=$*
 echo Diff
 git diff $FILE
 echo
 echo Recent changes:
 git log -n 3 --oneline --decorate $FILE
+#TODO - move and make a functionWq
+#TODO - moe / rehash
