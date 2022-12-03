cd40b32797feabc2715962ed0b835181a54c6540
Andrew Black <andrew@black1.org.uk>
2022-12-03 15:06:21 +0000

    ADD fnction clipf

--- aliases_clip.sh
+++ aliases_clip.sh
@@ -4,6 +4,12 @@ OFFSET=$1
 echo -n $(date.py $OFFSET) | xclip -i -selection clipboard
 }
 
+function clipf() {
+  FILE=$1
+  readlink ./$FILE -f | xclip -i -selection clipboard
+  echo clip : $( xclip -o -selection clipboard)
+
+}
 #  pipe | clip or clip >file>
     alias clip='xclip -i -selection clipboard'
     alias clips='xclip -o -selection clipboard'
