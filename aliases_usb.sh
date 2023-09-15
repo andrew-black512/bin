9c9f7c7ee1dbd944bf59892012c671cc16109bdd
Andrew Black <andrew@black1.org.uk>
2023-09-15 14:12:10 +0100

    new file:   aliases_usb.sh

---
+++ aliases_usb.sh
@@ -0,0 +1,48 @@
+# not as general as it might be....
+USB=/media/andrew/u_stripey
+MEDIA=/media/andrew
+
+
+function usbname {
+   ls /home/andrew/scratch/usbtest/
+}
+#general
+function uslist {
+   echo
+   echo $MEDIA ...
+   ls -1 $MEDIA
+   echo
+
+}
+
+#specific to current disk
+alias udm='umount $USB'
+alias lsflash='ls /media/andrew'
+alias cdflash='cd /media/andrew/*'
+
+function umountme {
+
+   # TODO handle case of file in /.. better
+   # TODO allow a paramer, defaulting to .
+   # https://unix.stackexchange.com/questions/90252/how-to-get-mount-point-of-filesystem-containing-given-file
+
+   DEV=$(df -h . |tail -1 | awk -F% '{print $NF}' | tr -d ' ' )
+   cd /media/andrew 
+   echo trying to unmount $DEV 
+   umount $DEV -v
+}
+function umountfile {
+
+   # TODO allow a paramer, defaulting to .
+
+   DEV=$(df -h $1 |tail -1 | awk -F% '{print $NF}' | tr -d ' ' )
+   cd /media/andrew 
+   echo trying to unmount $DEV 
+   umount $DEV -v
+}
+
+
+# deviec info
+ alias webc="lsusb -s 1:2"
+
+ alias findusb='find  /media/andrew/ -mtime -1'
