bac998f28c0523f973749393a00e3404b1457a9c
Andrew Black <andrew@black1.org.uk>
2025-01-21 17:37:00 +0000

    added funcs to weekly

--- aliases_weekly.sh
+++ aliases_weekly.sh
@@ -1,16 +1,19 @@
-# CURR=/home/andrew/scratch/25w03
-# TODO - sep
-alias rant='cat >> $CURR/rant'
-alias todo='cat >> $CURR/todo'
+# WEEK=/home/andrew/scratch/25w03
+# defined in aliases_auto.sh to make it easier to overide
 
-alias gcur='cd $CURR;pwd'
-alias gwork='cd $CURR/work;pwd'
-alias treecur='tree $CURR'
+alias rant='cat >> $WEEK/rant'
+alias todo='cat >> $WEEK/todo'
+
+alias gweek='cd $WEEK;pwd'
+alias gwork='cd $WEEK/work;pwd'
+alias gdraft='cd $WEEK/draft;pwd'
+
+alias treecur='tree $WEEK'
 function weekly {
-    echo "initalising weekley dir $CURR"
-    mkdir -p $CURR/draft
-    mkdir -p $CURR/work
-    cd $CURR/work
+    echo "initalising weekly dir $WEEK"
+    mkdir -p $WEEK/draft
+    mkdir -p $WEEK/work
+    cd $WEEK/work
     git init
     touch test.txt
     git add test.txt
