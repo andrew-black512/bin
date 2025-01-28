9db12905839f574af2c85e5e9b34d77eeab560b6
Andrew Black <andrew@black1.org.uk>
2025-01-28 08:25:08 +0000

    commentsO

--- dodot.sh
+++ dodot.sh
@@ -1,10 +1,9 @@
 #TODO - only use out dir if it exists
 #TODO - should it use out in directory pf dot file or current dir?
-##   perl station2dot.pl LBG_PUR > out/L.do
-
-#TODO handle other directoires. Either use regexp to removed or compose from dirname.
 FILEBASE=`basename $1 .dot`
-PDF=out/${FILEBASE}.pdf
+OUTDIR=~/scratch/diag_pdf
+PDF=$OUTDIR/${FILEBASE}.pdf
+#ls $OUTDIR
 
    if dot ${FILEBASE}.dot -Tpdf > ${PDF} ;then
       gnome-open ${PDF}
