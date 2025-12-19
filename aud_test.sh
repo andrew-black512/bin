5df7d69934b2ea4ebe54cc55632c4490b64cdcd6
Andrew Black <andrew@black1.org.uk>
2025-12-19 13:00:41 +0000

    use rec ommaand

--- aud_test.sh
+++ aud_test.sh
@@ -4,5 +4,6 @@ FILE=/tmp/soxrecording.wav
 play $ANO 
 # TODO make sensible name
 echo Recording 
- sox -b 32 -e unsigned-integer -r 96k -c 2 -d --clobber --buffer $((96000*2*10)) $FILE trim 0 3
+# sox -b 32 -e unsigned-integer -r 96k -c 2 -d --clobber --buffer $((96000*2*10)) $FILE trim 0 3
+rec $FILE trim 0 3 
 play $FILE
